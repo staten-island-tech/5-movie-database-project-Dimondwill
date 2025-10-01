d9074da9f4649d99e78cea9c0fd846ef64ae9cea
Dimondwill <damianr6564@nycstudents.net>
2025-10-01 10:22:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ import json
 ## Open the JSON file of movie data
 movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
-data = json.load(movies)
+data = json.load(movies)\
+x = input("oh hey")
+for i in range (15272):
+    print({data["year" > ]["title"]})            
 
