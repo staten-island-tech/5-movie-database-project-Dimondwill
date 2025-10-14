42f2a6d9d0e03c71fc2f95ad29ad5b902abc2911
Dimondwill <damianr6564@nycstudents.net>
2025-10-14 10:14:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import json
 movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
-year = int(input("oh hey give me a year and ill tell you all the movies that came out after it: "))
+'''year = int(input("oh hey give me a year and ill tell you all the movies that came out after it: "))
 yea = int(input("now give me a year and ill tell you all the movies before it: "))
 ye = int(input("same thing but noe ill tell you all the movies that came out that year"))
-titl = input("search for the exact title of a movie: ")
+titl = str(input("search for the exact title of a movie: "))'''
+gen = list(input("genre: "))
 
 for i in range (len(data)):
     '''if (data[i]['year']) > year:
@@ -15,5 +16,7 @@ for i in range (len(data)):
         print({data[i]['title']})
     if(data[i]['year']) ==  ye:
         print({data[i]['title']})'''
-    if(data[i]['title']) == titl:
+    '''if(data[i]['title']) == titl:
+        print({data[i]['title']})'''
+    if(data[i]['genres']) == gen:
         print({data[i]['title']})
